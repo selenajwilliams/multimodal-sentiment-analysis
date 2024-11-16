a7fe88dc05943e2932a8d7a60ea0e3da9fd3c699
selenajwilliams <selena_williams@brown.edu>
2024-11-16 12:51:49 -0500

    [feat] added script to extract subset of data

--- main.py
+++ main.py
@@ -32,39 +32,62 @@ def init_directories(save_path, t_options, modalities, sentiments, categories):
     # print(f'created the following new directories: ')
     # [print(dir) for dir in new_directories]
 
+def fix_data():
+
+    t_options = ['train', 'test']
+    modalities = ['image', 'text']
+    sentiments = ['positive', 'negative']
+    categories = ['bady']
+
+    for t in t_options:
+        for modality in modalities:
+            for sentiment in sentiments:
+                for category in categories:
+                    folder_path = f'../MASAD/{t}/{modality}/{sentiment}/{category}'
+                    if os.path.exists(folder_path):
+                        new_path = f'../MASAD/{t}/{modality}/{sentiment}/baby'
+                        os.rename(folder_path, new_path)
+                        print(f'renamed {folder_path} to end in baby')
+
+
+
+
+
 def extract_data_subset():
 
     save_path = 'data'
     t_options = ['train', 'test']
     modalities = ['image', 'text']
     sentiments = ['positive', 'negative']
-    categories = ['apple', 'autumn', 'bady']
+    categories = ['baby', 'autumn', 'apple']
 
     # go through existing dataset and save subset of data to new folders
     for t in t_options:
-        for modality in modalities:
-            for sentiment in sentiments:
-                for category in categories:
-                    if category == 'autumn':
-                        break
-                    folder_path = f'../MASAD/{t}/{modality}/{sentiment}/{category}'
-                    print(f'extracting the first 5 images at {folder_path}...')
-                    files = os.listdir(folder_path)
-                    # print(f'  first 5 files: ')
-                    # [print(f'    {file}') for file in files[:5]]
-                    for i in range(5):
-                        file_name = files[i].rsplit('/')[0]
-                        src_path = f'{folder_path}/{file_name}'
-                        # print(f'file name: {file_name}')
-                        print(f'src path: {src_path}')
-                        
-                        dest_path = f'{save_path}/{t}/{modality}/{sentiment}/{category}/{file_name}'
-                        # print(f'dest path: {dest_path}')
+        for sentiment in sentiments:
+            for category in categories:
+                print(f'processing {category} {t} data')
+                folder_path = f'../MASAD/{t}/image/{sentiment}/{category}'
+                print(f'extracting the first 5 images at {folder_path}...')
+                files = os.listdir(folder_path)
+                for i in range(5):
+                    id = files[i].rsplit('/')[0].split('.')[0]
+                    print(f'id: {id}')
+                    # copy image to new location
+                    img_src_path = f'{folder_path}/{id}.jpg'
+                    img_dest_path = f'{save_path}/{t}/image/{sentiment}/{category}/{id}.jpg'
+                    shutil.copy(img_src_path, img_dest_path)
 
-                        shutil.copy(src_path, dest_path)
+                    # copy corresponding caption to new location
+                    txt_src_path = f'../MASAD/{t}/text/{sentiment}/{category}/{id}.txt'
+                    txt_dest_path = f'{save_path}/{t}/text/{sentiment}/{category}/{id}.txt'
+                    shutil.copy(txt_src_path, txt_dest_path)
 
 
 
+print(f'running init directories')
+init_directories(save_path, t_options, modalities, sentiments, categories)
+print(f'running fix data')
+fix_data()
 extract_data_subset()
 
 
